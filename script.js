342070a65a009f94d64bbec28ef32cac9d33012c
Dania Algamdi <ap34p.0@gmail.com>
2025-10-01 18:38:47 +0300

    new projects

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-
 form.addEventListener("submit", function (e) {
-    if (password.value !== confirmPassword.value) {
-        e.preventDefault();
-        errorMessage.style.display = "block";
+    e.preventDefault(); // يمنع الانتقال التلقائي
+    if (firstName.value.trim() === "" || email.value.trim() === "") {
+      errorMessage.style.display = "block"; // إظهار الخطأ
     } else {
-        errorMessage.style.display = "none";
-        alert("Account created successfully!");
+      errorMessage.style.display = "none"; // إخفاء الخطأ
+      window.location.href = "index.html"; // ✅ الانتقال للرئيسية
     }
-})
+    
+  });
